1174d61e6c364bf6cb4fc5cdd5d8ba2b16ed0ac1
yuanjun <yuanjun@papayamobile.com>
2018-05-12 18:31:42 +0800

    123

--- lib/tools/message_controller.py
+++ lib/tools/message_controller.py
@@ -81,6 +81,7 @@ class MessageController(object):
                 #     logging.info("create_response_iterator: %s" % response)
                 if response == "shelf_init" or response.SerializeToString().find("StreamMessage") == -1:
                     print "fuck you!!!!"
+                    print response.SerializeToString(), type(response.SerializeToString())
                     if response == "shelf_init":
                         self._request_queue.put(response)
                         continue 
