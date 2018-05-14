641d38ad8a2067b191fb3bde9042bd7587098ba1
yuanjun <yuanjun@papayamobile.com>
2018-05-14 11:19:48 +0800

    led

--- lib/data/light.py
+++ lib/data/light.py
@@ -8,38 +8,46 @@ class Light(object):
     def __init__(self):
         self.is_open = False
         self.open_time = -1
+        self.close_time = -1
         self.lib = ctypes.cdll.LoadLibrary("./libled.so")
         self.func = self.lib.LED_Board_init
         self.func.restype = c_int
         print "led init ret:"
         self.fd = self.func()
         print self.fd
-        self.open_func = self.lib.Open_Lights
-        self.open_func.restype = c_int
-        self.close_func = self.lib.Close_Lights
-        self.close_func.restype = c_bool
 
-    def open_light(self):
+    def open_light(self, led_id):
+        open_func = self.lib.Open_Lights
+        open_func.restype = c_int
+        self.open_time = time.time()
+        open_func(self.fd, led_id)
+
+    def close_light(self, led_id):
+        close_func = self.lib.Close_Lights
+        close_func.restype = c_bool
+        close_func(self.fd, led_id)
+
+    def open_all_light(self):
+        self.open_light(1)
+        self.open_light(2)
+        self.open_light(3)
         self.is_open = True
         self.open_time = time.time()
-        self.open_func(self.fd, 1)
-        self.open_func(self.fd, 2)
-        self.open_func(self.fd, 3)
-        time.sleep(1)
 
-    def close_light(self):
+    def close_all_light(self):
+        self.close_light(1)
+        self.close_light(2)
+        self.close_light(3)
         self.is_open = False
-        self.close_func(self.fd, 1)
-        self.close_func(self.fd, 2)
-        self.close_func(self.fd, 3)
+        self.close_time = time.time()
 
-    def auto_check(self):
-        if self.is_open and time.time() - self.open_time > 300:
-            self.close_light()
+    # def auto_check(self):
+    #     if self.is_open and time.time() - self.open_time > 300:
+    #         self.close_light()
 
 
 if __name__ == "__main__":
     led = Light()
-    led.close_light()
+    led.open_all_light()
 
 
