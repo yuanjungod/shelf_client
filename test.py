d47701587bb2851626e44caf0d347898704e1278
yuanjun <yuanjun@papayamobile.com>
2018-05-14 13:43:30 +0800

    asd

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import urllib
 import cv2
 
 # url = "http://10.12.3.65/resources/slots_theme/theme98/1.png"
-url = "http://192.168.1.13:8888/cars/Aston-one-77.png"
+url = "http://192.168.1.180:8888/cars/Aston-one-77.png"
 resp = urllib.urlopen(url)
 image = np.asarray(bytearray(resp.read()), dtype="uint8")
 print image
