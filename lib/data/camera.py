6e9f591e51a7fec641f3148e6a3faf376dccb571
yuanjun <yuanjun@papayamobile.com>
2018-05-15 13:32:58 +0800

    shelf

--- lib/data/camera.py
+++ lib/data/camera.py
@@ -148,7 +148,7 @@ class Camera(object):
                             images=[device_gateway_pb2.MessageSenseData.Image(
                                 local_path="http://192.168.1.180:8888/images/%s/%s-%s.jpg" % (
                                     datetime.date.today(), photo_time, i)) for i in range(len(frame_list))]))
-                        sense_data = device_gateway_pb2.StreamMessage(id=str(time.time()), payload=any)
+                        sense_data = device_gateway_pb2.StreamMessage(payload=any)
                         logging.debug(sense_data)
                         self.return_cmd_queue.put(sense_data)
 
