2be5494550680ef3171a471fd206bd0b6744e338
yuanjun <yuanjun@papayamobile.com>
2018-05-15 10:55:50 +0800

    all

--- lib/data/shelf.py
+++ lib/data/shelf.py
@@ -54,85 +54,86 @@ class Shelf(object):
 
         if str(type(request)).find("StreamMessage") != -1:
             logging.info("process_request: %s" % request.payload.type_url)
-        else:
-            logging.info("process_request: %s" % type(request))
+            if request.payload.type_url.find("MessageUnlockDoor") != -1:
+                logging.debug("MessageUnlockDoor")
+                if request.id != "":
+                    self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
+                logging.debug("check device")
+                if self.check_device():
 
-        if request == "shelf_init":
+                    self.light.open_all_light()
+                    open_result = self.device.open_lock()
+                    logging.debug("open lock: %s" % open_result)
+                    if not open_result:
+                        if request.id != "":
+                            any = any_pb2.Any()
+                            any.Pack(device_gateway_pb2.MessageDeviceState(lock=3))
+                            self._queue.put(device_gateway_pb2.StreamMessage(id=str(time.time()), payload=any))
+                    else:
+                        self.in_use = True
+                        logging.debug("################in use################")
+                        try_count = 5
+                        door_status = self.device.door_status.next()
+                        while not door_status and try_count > 0:
+                            door_status = self.device.door_status.next()
+                            try_count -= 1
+                            time.sleep(1)
+
+                        if door_status:
+                            self.shelf_display([3, {"open": 1}])
+                            self.camera.push_frames_to_server(request)
+                        else:
+                            self.device.lock_lock()
+                            any = any_pb2.Any()
+                            any.Pack(device_gateway_pb2.MessageDoorClosed())
+                            self._queue.put(device_gateway_pb2.StreamMessage(id=str(time.time()), payload=any))
+                logging.debug(
+                    "#############################fuck#############################%s, %s" % (self.is_init, self.in_use))
+
+            elif request.payload.type_url.find("MessageRefreshCode") != -1:
+                logging.debug("MessageRefreshCode")
+                if request.id != "":
+                    self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
+                self._queue.put(device_gateway_pb2.AuthorizationRequest())
+            elif request.payload.type_url.find("MessageCreateDeviceToken") != -1:
+                logging.debug("MessageCreateDeviceToken&*********************************: %s" % request)
+                message_code_used = device_gateway_pb2.MessageCreateDeviceToken()
+                request.payload.Unpack(message_code_used)
+                if message_code_used.biz_name == "bind-device":
+                    pass
+                else:
+                    self.client_config["device_token"] = message_code_used.device_token
+                    with open("config.json", "w") as f:
+                        json.dump(self.client_config, f)
+                    self.shelf_display([
+                        4, {"device_token": message_code_used.device_token, "biz_name": message_code_used.biz_name}])
+                    if self.in_use is False and self.is_init is True:
+                        self.in_use = True
+                        self.shelf_display([3, {"open": 1}])
+                        self.light.open_light()
+                        self.camera.push_frames_to_server(request)
+                        self.in_use = True
+
+                    self.scan_start = time.time()
+                    if request.id != "":
+                        self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
+            elif request.payload.type_url.find("MessageRevokeDeviceToken") != -1:
+                self.in_use = False
+                logging.debug("MessageRevokeDeviceToken")
+                if request.id != "":
+                    self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
+                logging.debug(type(device_gateway_pb2.AuthorizationRequest()))
+                self._queue.put(device_gateway_pb2.AuthorizationRequest())
+            else:
+                logging.info("process_request: %s" % type(request))
+        elif request == "shelf_init":
             if self.in_use is False:
                 self.light.open_all_light()
                 self.camera.push_frames_to_server(request)
-        elif str(type(request)).find("AuthorizationRequest"):
+        elif str(type(request)).find("AuthorizationRequest") != -1:
             logging.debug("AuthorizationRequest")
             self._queue.put(request)
-        elif request.payload.type_url.find("MessageUnlockDoor") != -1:
-            logging.debug("MessageUnlockDoor")
-            if request.id != "":
-                self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
-            logging.debug("check device")
-            if self.check_device():
 
-                self.light.open_all_light()
-                open_result = self.device.open_lock()
-                logging.debug("open lock: %s" % open_result)
-                if not open_result:
-                    if request.id != "":
-                        any = any_pb2.Any()
-                        any.Pack(device_gateway_pb2.MessageDeviceState(lock=3))
-                        self._queue.put(device_gateway_pb2.StreamMessage(id=str(time.time()), payload=any))
-                else:
-                    self.in_use = True
-                    logging.debug("################in use################")
-                    try_count = 5
-                    door_status = self.device.door_status.next()
-                    while not door_status and try_count > 0:
-                        door_status = self.device.door_status.next()
-                        try_count -= 1
-                        time.sleep(1)
-
-                    if door_status:
-                        self.shelf_display([3, {"open": 1}])
-                        self.camera.push_frames_to_server(request)
-                    else:
-                        self.device.lock_lock()
-                        any = any_pb2.Any()
-                        any.Pack(device_gateway_pb2.MessageDoorClosed())
-                        self._queue.put(device_gateway_pb2.StreamMessage(id=str(time.time()), payload=any))
-            logging.debug("#############################fuck#############################%s, %s" % (self.is_init, self.in_use))
-
-        elif request.payload.type_url.find("MessageRefreshCode") != -1:
-            logging.debug("MessageRefreshCode")
-            if request.id != "":
-                self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
-            self._queue.put(device_gateway_pb2.AuthorizationRequest())
-        elif request.payload.type_url.find("MessageCreateDeviceToken") != -1:
-            logging.debug("MessageCreateDeviceToken&*********************************: %s" % request)
-            message_code_used = device_gateway_pb2.MessageCreateDeviceToken()
-            request.payload.Unpack(message_code_used)
-            if message_code_used.biz_name == "bind-device":
-                pass
-            else:
-                self.client_config["device_token"] = message_code_used.device_token
-                with open("config.json", "w") as f:
-                    json.dump(self.client_config, f)
-                self.shelf_display([
-                    4, {"device_token": message_code_used.device_token, "biz_name": message_code_used.biz_name}])
-                if self.in_use is False and self.is_init is True:
-                    self.in_use = True
-                    self.shelf_display([3, {"open": 1}])
-                    self.light.open_light()
-                    self.camera.push_frames_to_server(request)
-                    self.in_use = True
-
-                self.scan_start = time.time()
-                if request.id != "":
-                    self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
-        elif request.payload.type_url.find("MessageRevokeDeviceToken") != -1:
-            self.in_use = False
-            logging.debug("MessageRevokeDeviceToken")
-            if request.id != "":
-                self._queue.put(device_gateway_pb2.StreamMessage(reply_to=request.id))
-            logging.debug(type(device_gateway_pb2.AuthorizationRequest()))
-            self._queue.put(device_gateway_pb2.AuthorizationRequest())
 
 
 
