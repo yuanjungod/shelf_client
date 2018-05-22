07244b21e792717127532f176b08c9bc15bbdba8
yuanjun <yuanjun@papayamobile.com>
2018-05-22 10:38:00 +0800

    s

--- time.sh
+++ time.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 sudo chmod 666 /dev/parport0
 sudo chmod 666 /dev/ttyS0
 pid=`ps -ef | grep "sudo nohup python client.py" | grep -v "grep" | awk -F \  {'print $2'}`
-if [[ $pid == "" ]]
+if [ "$pid"x == ""x ]
 then
 sudo nohup python client.py &
 fi
