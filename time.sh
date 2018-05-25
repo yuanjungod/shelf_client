c4a181efd69f4bb7ef197c0ba1ae0004809c7cb6
yuanjungod <quantum_matrix@sina.com>
2018-05-25 14:05:48 +0800

    time.sh

--- time.sh
+++ time.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+echo "fucking start" >> /home/gxm/code/shelf_client/test.tex
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-sudo chmod 666 /dev/parport0
-sudo chmod 666 /dev/ttyS0
+chmod 666 /dev/parport0
+chmod 666 /dev/ttyS0
 pid=`ps -ef | grep "sudo nohup python client.py" | grep -v "grep" | awk -F \  {'print $2'}`
-if [[ $pid == "" ]]
-then
-sudo nohup python client.py &
+if [ "$pid" = "" ] ; then
+nohup /usr/bin/python /home/gxm/code/shelf_client/client.py &
+echo "running" >> /home/gxm/code/shelf_client/test.tex
+# rm -rf *.log
 fi
+echo "fucking finished" >> /home/gxm/code/shelf_client/test.tex
