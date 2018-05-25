5ba4a402d6b14012397d1badadaf7d166193979f
yuanjun <yuanjun@papayamobile.com>
2018-05-25 14:43:53 +0800

    asd

--- time.sh
+++ time.sh
@@ -6,7 +6,7 @@ chmod 666 /dev/parport0
 chmod 666 /dev/ttyS0
 pid=`ps -ef | grep "/usr/bin/python /home/gxm/code/shelf_client" | grep -v "grep" | awk -F \  {'print $2'}`
 if [ "$pid" = "" ] ; then
-nohup /usr/bin/python /home/gxm/code/shelf_client/cron_test.py &
+nohup /home/gxm/code/shelf_client/env/bin/python /home/gxm/code/shelf_client/cron_test.py &
 echo "running" >> /home/gxm/code/shelf_client/test.tex
 # rm -rf *.log
 fi
