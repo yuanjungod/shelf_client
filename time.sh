0c69652533a254c2ce9d2b8263ce0f13fd858f8f
yuanjun <yuanjun@papayamobile.com>
2018-05-25 14:15:55 +0800

    test

--- time.sh
+++ time.sh
@@ -5,7 +5,7 @@ chmod 666 /dev/parport0
 chmod 666 /dev/ttyS0
 pid=`ps -ef | grep "sudo nohup python client.py" | grep -v "grep" | awk -F \  {'print $2'}`
 if [ "$pid" = "" ] ; then
-nohup /usr/bin/python /home/gxm/code/shelf_client/client.py &
+/usr/bin/python /home/gxm/code/shelf_client/cron_test.py
 echo "running" >> /home/gxm/code/shelf_client/test.tex
 # rm -rf *.log
 fi
