3e247226303e08db1d5bf7132b53e6e771a9ae52
yuanjun <yuanjun@papayamobile.com>
2018-05-25 14:55:45 +0800

    asd

--- time.sh
+++ time.sh
@@ -4,7 +4,7 @@ echo "fucking start" >> /home/gxm/code/shelf_client/test.tex
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 chmod 666 /dev/parport0
 chmod 666 /dev/ttyS0
-pid=`ps -ef | grep "/usr/bin/python /home/gxm/code/shelf_client" | grep -v "grep" | awk -F \  {'print $2'}`
+pid=`ps -ef | grep "/home/gxm/code/shelf_client/env/bin/python /home/gxm/code/shelf_client" | grep -v "grep" | awk -F \  {'print $2'}`
 if [ "$pid" = "" ] ; then
 nohup /home/gxm/code/shelf_client/env/bin/python /home/gxm/code/shelf_client/cron_test.py &
 echo "running" >> /home/gxm/code/shelf_client/test.tex
