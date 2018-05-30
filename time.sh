f20b908c0c9e2632e11ecbb66a9c8612fa828991
yuanjungod <quantum_matrix@sina.com>
2018-05-30 16:01:55 +0800

    time.sh

--- time.sh
+++ time.sh
@@ -4,10 +4,11 @@
 echo "ml frame server start" >> /home/gxm/code/shelf_client/test.tex
 pid=`ps -ef | grep "/home/gxm/code/ML_framework_express" | grep -v "grep" | awk -F \  {'print $2'}`
 if [ "$pid" = "" ] ; then
+cd /home/gxm/code/ML_framework_express/
+git pull origin matser
 cp -rf /home/gxm/code/ML_framework_express/lib /home/gxm/code/ML_framework_express/env/lib/python2.7/site-packages/
 cp -rf /home/gxm/code/ML_framework_express/models /home/gxm/code/ML_framework_express/env/lib/python2.7/site-packages/
 nohup /home/gxm/code/ML_framework_express/env/bin/python /home/gxm/code/ML_framework_express/offline_server_run.py &
-echo "ML frame server running" >> /home/gxm/code/shelf_client/test.tex
 # rm -rf *.log
 fi
 echo "ml frame server finished" >> /home/gxm/code/shelf_client/test.tex
@@ -18,7 +19,6 @@ pid=`ps -ef | grep "python -m SimpleHTTPServer 8888" | grep -v "grep" | awk -F \
 if [ "$pid" = "" ] ; then
 cd /home/gxm/code/shelf_client
 nohup python -m SimpleHTTPServer 8888 &
-echo "image server running" >> /home/gxm/code/shelf_client/test.tex
 # rm -rf *.log
 fi
 echo "fucking finished" >> /home/gxm/code/shelf_client/test.tex
@@ -30,6 +30,8 @@ chmod 666 /dev/ttyS0
 echo "client start" >> /home/gxm/code/shelf_client/test.tex
 pid=`ps -ef | grep "/home/gxm/code/shelf_client/env/bin/python /home/gxm/code/shelf_client" | grep -v "grep" | awk -F \  {'print $2'}`
 if [ "$pid" = "" ] ; then
+cd /home/gxm/code/shelf_client/
+git pull origin dev
 cp -rf /home/gxm/code/shelf_client/lib /home/gxm/code/shelf_client/env/lib/python2.7/site-packages/
 cp -rf /home/gxm/code/shelf_client/device_back /home/gxm/code/shelf_client/env/lib/python2.7/site-packages/device
 nohup /home/gxm/code/shelf_client/env/bin/python /home/gxm/code/shelf_client/client.py &
